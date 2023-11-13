13ca2d2e538c54695217bbd1fdf7e133d9cb1628
ian <iandkaneshiro@gmail.com>
2023-11-13 10:36:26 -0800

    learning about webpack with Typescriopt

---
+++ webpack_ts/dist/index.js
@@ -0,0 +1,10 @@
+import Dog from "./Dog.js";
+import ShelterDog from "./ShelterDog.js";
+import { add, multiply, divide } from "./utils.js";
+console.log("From index file!");
+const elton = new Dog("Elton", "Aussie", 19);
+elton.bark();
+const buffy = new ShelterDog("Buffy", "Pitbull", 4, "Nice place");
+console.log(add(4, 5));
+console.log(multiply(2, 2));
+console.log(divide(10, 2));
